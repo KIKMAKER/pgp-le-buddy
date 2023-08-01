5c7959a241a4caecb6309121bd1c4733c8f54790
Dewald Reynecke <dewaldreynecke@me.com>
2023-08-01 11:37:42 +0200

    add instructions to env seed files

---
+++ db/seeds/development.rb
@@ -0,0 +1,9 @@
+# Add the seed actions for the development environment to this file.
+
+puts "Development seed running."
+
+# ---------------------- Add your seed code in this block ----------------------
+
+# ------------------------------------------------------------------------------
+
+puts "Development seed complete."
---
+++ db/seeds/test.rb
@@ -0,0 +1,9 @@
+# Add the seed actions for the test environment to this file.
+
+puts "Test seed running."
+
+# ---------------------- Add your seed code in this block ----------------------
+
+# ------------------------------------------------------------------------------
+
+puts "Test seed complete."
