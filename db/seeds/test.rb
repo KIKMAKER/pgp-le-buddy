1113f41c1e0d7ffa79ef26fd59e585fa5c45e3d9
Dewald Reynecke <dewaldreynecke@me.com>
2023-08-01 15:22:42 +0200

    update instructions in test.rb seed

--- db/seeds/test.rb
+++ db/seeds/test.rb
@@ -1,9 +1,5 @@
-# Add the seed actions for the test environment to this file.
+# DO NOT USE THIS FILE FOR TESTING
 
-puts "Test seed running."
-
-# ---------------------- Add your seed code in this block ----------------------
-
-# ------------------------------------------------------------------------------
-
-puts "Test seed complete."
+# Ordinarily you'd seed your testing environment here, but for this project
+# we will not have a dedicated testing environment. Please check the project
+# documentation for more information on testing strategies.
