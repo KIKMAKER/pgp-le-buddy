ea9db528c5ab35b37b2332cb017a9b9a34e7bd62
Dewald Reynecke <dewaldreynecke@me.com>
2023-08-03 17:33:31 +0200

    Seed Kitt challenges

--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -1,8 +1,12 @@
 # Add the seed actions for the development environment to this file.
-
 puts "Development seed running."
 
 # ---------------------- Add your seed code in this block ----------------------
+# Clear existing data from database
+Challenge.destroy_all
+
+# Seed the Kitt Challenges
+load(Rails.root.join( 'db', 'seeds', 'partials', '_challenges.rb'))
 
 # ------------------------------------------------------------------------------
 
