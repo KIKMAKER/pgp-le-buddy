771a4171e920f009ddc28385a829feb795be1093
Dewald Reynecke <dewaldreynecke@me.com>
2023-08-04 10:54:44 +0200

    add validations to BuddyUp

--- app/models/buddy_up.rb
+++ app/models/buddy_up.rb
@@ -1,4 +1,7 @@
 class BuddyUp < ApplicationRecord
   belongs_to :user
   belongs_to :challenge
+
+  validates :name, :description, :auto_match, :challenge, :user, presence: true
+  enum :status, [:active, :archived, :complete]
 end
