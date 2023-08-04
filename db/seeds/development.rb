bb7b98291366a44590c53dffc7a7725ab03744f2
Dewald Reynecke <dewaldreynecke@me.com>
2023-08-04 16:22:16 +0200

    create seed for AppFeedback

--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -4,6 +4,7 @@ puts "Development seed running."
 # ---------------------- Add your seed code in this block ----------------------
 # Clear existing data from database
 Challenge.destroy_all
+AppFeedback.destroy_all
 
 # Test user account
 User.create(email: "test@test.com", password: "123456")
@@ -12,6 +13,11 @@ puts "Test user created: test@test.com, 123456"
 # Kitt Challenges
 load(Rails.root.join( 'db', 'seeds', 'partials', '_challenges.rb'))
 
+# AppFeedbacks
+puts "Creating AppFeedbacks..."
+load(Rails.root.join( 'db', 'seeds', 'partials', '_app_feedbacks.rb'))
+puts "...done. #{AppFeedback.all.count} AppFeedbacks created."
+puts ""
 # ------------------------------------------------------------------------------
 
 puts "Development seed complete."
---
+++ db/seeds/partials/_app_feedbacks.rb
@@ -0,0 +1,20 @@
+app_feedback = AppFeedback.new(title: "Slow!!!",
+                               contents: "This app is not nearly fast enough.
+                                         Kitt sets a standard you should aspire
+                                         to.")
+app_feedback.user = User.all.sample
+app_feedback.datetime = Time.now - 20000000
+app_feedback.save
+
+app_feedback = AppFeedback.new(title: "So pretty",
+                               contents: "Thank you for making a beautifull app.")
+app_feedback.user = User.all.sample
+app_feedback.datetime = Time.now - 15000000
+app_feedback.save
+
+app_feedback = AppFeedback.new(title: "Broken link",
+                               contents: "When I click on Messages in the nav
+                                         it goes to my profile page.")
+app_feedback.user = User.all.sample
+app_feedback.datetime = Time.now - 10000000
+app_feedback.save
