45983715fc0c25a5ab2b7beadb06111984fdf7f7
Dewald Reynecke <dewaldreynecke@me.com>
2023-08-04 16:02:48 +0200

    handle index, new, create for AppFeedbacks

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  get 'app_feedbacks/index'
-  get 'app_feedbacks/new'
-  get 'app_feedbacks/create'
-  get 'app_feedbacks/edit'
-  get 'app_feedbacks/update'
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: "pages#home"
 
