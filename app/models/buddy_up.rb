2e11bdab85f1f64e2a85aac3fdf628258e7d1b52
Dewald Reynecke <dewaldreynecke@me.com>
2023-08-07 08:57:47 +0200

    set default enum for BuddyUp status

--- app/models/buddy_up.rb
+++ app/models/buddy_up.rb
@@ -3,5 +3,5 @@ class BuddyUp < ApplicationRecord
   belongs_to :challenge
 
   validates :name, :description, :challenge, :user, presence: true
-  enum :status, [:active, :archived, :complete]
+  enum :status, { active: 0, archived: 1, complete: 2 }, default: :active
 end
--- db/seeds/partials/_buddy_ups.rb
+++ db/seeds/partials/_buddy_ups.rb
@@ -3,28 +3,23 @@ buddy_up = BuddyUp.new(name: "It did not come together in Rails.", description: 
 buddy_up.user = User.where(email: "shane@shane.com").first
 buddy_up.challenge = Challenge.where(["module = ? and name = ?", "Rails", "Stupid Coaching"]).first
 buddy_up.save
-buddy_up.active!
 buddy_up = BuddyUp.new(name: "Random code things!", description: "Just feel like trying out a random challenge so I chose this one. See ya soon!!", auto_match: false, availability: "Mondays and Wednesdays, 11:00 to 16:00", favourites: 2)
 buddy_up.user = User.all.sample
 buddy_up.challenge = Challenge.all.sample
 buddy_up.save
-buddy_up.active!
 # Auto-match true and status active
 buddy_up = BuddyUp.new(name: "Pick me, pick me", description: "This challenge really got me during the bootcamp so I want to give it another try.", auto_match: true, availability: "Fridays, just after midnight for an hour or two", favourites: 0)
 buddy_up.user = User.all.sample
 buddy_up.challenge = Challenge.all.sample
 buddy_up.save
-buddy_up.active!
 buddy_up = BuddyUp.new(name: "For those idle evenings", description: "I have some free time in the evenings and thought I'd fill the time with some challenges. This particular one was chosen at random.", auto_match: true, availability: "Weekday evenings after 7pm until late", favourites: 1)
 buddy_up.user = User.all.sample
 buddy_up.challenge = Challenge.all.sample
 buddy_up.save
-buddy_up.active!
 buddy_up = BuddyUp.new(name: "Catching up", description: "After bootcamp I had to return to my job and never really had time to code. Looking to get back into it now by doing some of the challenges again.", auto_match: true, availability: "All day Sunday.", favourites: 4)
 buddy_up.user = User.all.sample
 buddy_up.challenge = Challenge.all.sample
 buddy_up.save
-buddy_up.active!
 # Auto-match true and status archived
 buddy_up = BuddyUp.new(name: "Help me help myself", description: "Have been struggling with this challenge for a while but just cannot figure it out. I have forked it on Github and if you join we can just jump in since I have the basics in there already.", auto_match: true, availability: "Weekends", favourites: 94)
 buddy_up.user = User.all.sample
