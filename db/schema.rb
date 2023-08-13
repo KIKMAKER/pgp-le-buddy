3d47a80019d354ce095a55c765a1d15f37cf0d95
Dewald Reynecke <dewaldreynecke@me.com>
2023-08-13 12:56:28 +0200

    remove content column from Feedback

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_13_102621) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_13_105536) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -49,7 +49,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_13_102621) do
   end
 
   create_table "feedbacks", force: :cascade do |t|
-    t.text "content"
     t.boolean "work_again", null: false
     t.bigint "buddy_up_id", null: false
     t.bigint "user_id", null: false
