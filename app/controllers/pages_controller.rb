a486a847e66c3d9a8c9200474c33f9609f6e60bf
Dewald Reynecke <dewaldreynecke@me.com>
2023-09-04 22:29:28 +0200

    create dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,4 +5,18 @@ class PagesController < ApplicationController
   def terms; end
   def privacy; end
   def team; end
+
+  def dashboard
+    @profile = current_user.profile
+    @active_bups = BuddyUp.active_list(@profile)
+    @requested_bups = Request.requested(@profile)
+    @request_success_rate = Request.success_rate(@profile)
+    @complete_bups = BuddyUp.complete_list(@profile)
+    @archive_bups = BuddyUp.archive_list(@profile)
+    @progress = BuddyUp.progress(@profile)
+    @count = BuddyUp.active_complete_total(@profile)
+    @all_count = BuddyUp.all_count(@profile)
+    @abandonment = BuddyUp.abandonment(@profile)
+    # TODO: still need to add the favourites
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
   resources :challenges, only: %i[index]
   resources :app_feedbacks
-  resources :buddy_ups, only: %i[index]
+  resources :buddy_ups, only: %i[index show]
   resources :profiles, only: %i[index show new create]
   resources :social_links, only: :create
   resources :requests
