aa69938306c7d61ccef7cd46822e99ab85ce83fe
Ben Malandrinos <bmalandrinos@gmail.com>
2023-09-07 11:34:42 +0100

    added show method to buddyups controller

--- app/controllers/buddy_ups_controller.rb
+++ app/controllers/buddy_ups_controller.rb
@@ -3,4 +3,8 @@ class BuddyUpsController < ApplicationController
     # This renders the global "Marketplace" page.
     @buddy_ups = BuddyUp.where(status: "active")
   end
+
+  def show
+    @buddy_ups = BuddyUp.find(params[:id])
+  end
 end
