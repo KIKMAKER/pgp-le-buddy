a967d563a5cd88c7ff910f774ae41e4923afb92a
Jonathan Watkins <jonrw97@gmail.com>
2023-09-19 16:23:33 +0200

    after sign in path for

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  def after_sign_in_path_for(resource)
+    if @user.profile.batch.nil?
+      profile_path(@user.profile)
+    else
+      super
+    end
+  end
 end
