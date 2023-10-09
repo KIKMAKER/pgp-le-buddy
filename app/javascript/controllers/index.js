c92db5b1ed10da9e92217cfe016f7f1c151e7216
Ben Malandrinos <bmalandrinos@gmail.com>
2023-10-09 20:17:07 +0100

    added modal and working with stimulus

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -9,3 +9,6 @@ application.register("hello", HelloController)
 
 import NavbarController from "./navbar_controller"
 application.register("navbar", NavbarController)
+
+import ModalController from "./modal_controller"
+application.register("modal", ModalController)
