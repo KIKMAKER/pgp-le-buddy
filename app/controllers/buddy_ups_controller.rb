3397a16245ea00f67d4a1cc31fe9eaac72de59fe
Dewald Reynecke <dewaldreynecke@me.com>
2023-12-06 16:49:56 +0200

    create buddy_up new

--- app/controllers/buddy_ups_controller.rb
+++ app/controllers/buddy_ups_controller.rb
@@ -12,5 +12,34 @@ class BuddyUpsController < ApplicationController
   def show
     @buddy_up = BuddyUp.find(params[:id])
     @request = Request.new
+    @challenge = Challenge.all
+  end
+
+  def new
+    @source = params[:source]
+    @buddy_up = BuddyUp.new
+  end
+
+  def create
+    @buddy_up = BuddyUp.new(buddy_up_params)
+    @buddy_up.profile = current_user.profile
+    @buddy_up.challenge = Challenge.find(params[:buddy_up][:challenge_id])
+    if @buddy_up.save
+      if params[:source] == "buddy_up_index"
+        redirect_to buddy_ups_path
+      elsif params[:source] == "dashboard"
+        redirect_to dashboard_path
+      else
+        redirect_to root_path
+      end
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def buddy_up_params
+    params.require(:buddy_up).permit(:name, :description, :auto_match, :availability, :challenge)
   end
 end
